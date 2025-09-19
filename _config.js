b64a987536e2c81267c34ed9aca6a7769f068163
lenox-isindu <lenox1739@gmail.com>
2025-09-20 01:27:25 +0300

    changed mogo url and instaled mocha

--- _config.js
+++ _config.js
@@ -1,9 +1,9 @@
 var config = {}
 
-// Update to have your correct username and password
 config.mongoURI = {
-    production: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom?retryWrites=true&w=majority',
-    development: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom-dev?retryWrites=true&w=majority',
-    test: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom-test?retryWrites=true&w=majority',
+  production: 'mongodb+srv://moringa:1234@devops1.fqjmlb6.mongodb.net/darkroom?retryWrites=true&w=majority&appName=devops1',
+  development: 'mongodb+srv://moringa:1234@devops1.fqjmlb6.mongodb.net/darkroom-dev?retryWrites=true&w=majority&appName=devops1',
+  test: 'mongodb+srv://moringa:1234@devops1.fqjmlb6.mongodb.net/darkroom-test?retryWrites=true&w=majority&appName=devops1',
 }
-module.exports = config;
+
+module.exports = config
